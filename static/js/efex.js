2d039dc9770a123320d2bd4ab1d7dc3ce73e40a9
lxschutz <lxschutz@gmail.com>
2024-01-19 15:24:13 -0600

    with highlighting

--- static/js/efex.js
+++ static/js/efex.js
@@ -1,3 +1,4 @@
+
 // // addEventListener("mouseover", (event) =>{});
 // // onmouseover = (event) => {}
 
@@ -20,12 +21,239 @@
 // // );
 // Object.onmouseover= function(){test}
 
-document.getElementsByClassName("macrame").addEventListener("mouseover", mouseOver);
-document.getElementsByClassName("macrame").addEventListener("mouseout", mouseOut);
+// document.getElementsByClassName("macrame").addEventListener("mouseover", mouseOver);
+// document.getElementsByClassName("macrame").addEventListener("mouseout", mouseOut);
 
-function mouseOver() {
-    document.getElementsByClassName("macrame").style.color = "yellow"
+// function mouseOver() {
+//     document.getElementsByClassName("macrame").style.color = "yellow"
+// }
+// function mouseOut() {
+//     document.getElementsByClassName("macrame").style.color = "black"
+// }
+
+const hiLiteLabel = document.querySelector('#macrameLabel');
+const hiLiteImg = document.querySelector('#macrameImg');
+hiLiteLabel.addEventListener('mouseover', (e) => {
+    hiLiteLabel.style.outline = "5px solid yellow"
+    hiLiteImg.style.outline = "5px solid yellow"
+})
+hiLiteLabel.addEventListener('mouseout', (e) => {
+    hiLiteLabel.style.outline = ""
+    hiLiteImg.style.outline = ""
+})
+hiLiteImg.addEventListener('mouseover', (e) => {
+    hiLiteLabel.style.outline = "5px solid yellow"
+    hiLiteImg.style.outline = "5px solid yellow"
+})
+hiLiteImg.addEventListener('mouseout', (e) => {
+    hiLiteLabel.style.outline = ""
+    hiLiteImg.style.outline = ""
+})
+
+const homeHiLiteLabel = document.querySelector('#homeLabel');
+const homeHiLiteImg = document.querySelector('#homeImg');
+homeHiLiteLabel.addEventListener('mouseover', (e) => {
+    homeHiLiteLabel.style.outline = "5px solid yellow"
+    homeHiLiteImg.style.outline = "5px solid yellow"
+})
+homeHiLiteLabel.addEventListener('mouseout', (e) => {
+    homeHiLiteLabel.style.outline = ""
+    homeHiLiteImg.style.outline = ""
+})
+homeHiLiteImg.addEventListener('mouseover', (e) => {
+    homeHiLiteLabel.style.outline = "5px solid yellow"
+    homeHiLiteImg.style.outline = "5px solid yellow"
+})
+homeHiLiteImg.addEventListener('mouseout', (e) => {
+    homeHiLiteLabel.style.outline = ""
+    homeHiLiteImg.style.outline = ""
+})
+
+const hiLiteLabel3 = document.querySelector('#illinoisLabel');
+const hiLiteImg3 = document.querySelector('#illinoisImg');
+hiLiteLabel3.addEventListener('mouseover', (e) => {
+    hiLiteLabel3.style.outline = "5px solid yellow"
+    hiLiteImg3.style.outline = "5px solid yellow"
+})
+hiLiteLabel3.addEventListener('mouseout', (e) => {
+    hiLiteLabel3.style.outline = ""
+    hiLiteImg3.style.outline = ""
+})
+hiLiteImg3.addEventListener('mouseover', (e) => {
+    hiLiteLabel3.style.outline = "5px solid yellow"
+    hiLiteImg3.style.outline = "5px solid yellow"
+})
+hiLiteImg3.addEventListener('mouseout', (e) => {
+    hiLiteLabel3.style.outline = ""
+    hiLiteImg3.style.outline = ""
+})
+
+const hiLiteLabel4 = document.querySelector('#roomsLabel');
+const hiLiteImg4 = document.querySelector('#roomsImg');
+hiLiteLabel4.addEventListener('mouseover', (e) => {
+    hiLiteLabel4.style.outline = "5px solid yellow"
+    hiLiteImg4.style.outline = "5px solid yellow"
+})
+hiLiteLabel4.addEventListener('mouseout', (e) => {
+    hiLiteLabel4.style.outline = ""
+    hiLiteImg4.style.outline = ""
+})
+hiLiteImg4.addEventListener('mouseover', (e) => {
+    hiLiteLabel4.style.outline = "5px solid yellow"
+    hiLiteImg4.style.outline = "5px solid yellow"
+})
+hiLiteImg4.addEventListener('mouseout', (e) => {
+    hiLiteLabel4.style.outline = ""
+    hiLiteImg4.style.outline = ""
+})
+
+const hiLiteLabel5 = document.querySelector('#bugLabel');
+const hiLiteImg5 = document.querySelector('#bugImg');
+hiLiteLabel5.addEventListener('mouseover', (e) => {
+    hiLiteLabel5.style.outline = "5px solid yellow"
+    hiLiteImg5.style.outline = "5px solid yellow"
+})
+hiLiteLabel5.addEventListener('mouseout', (e) => {
+    hiLiteLabel5.style.outline = ""
+    hiLiteImg5.style.outline = ""
+})
+hiLiteImg5.addEventListener('mouseover', (e) => {
+    hiLiteLabel5.style.outline = "5px solid yellow"
+    hiLiteImg5.style.outline = "5px solid yellow"
+})
+hiLiteImg5.addEventListener('mouseout', (e) => {
+    hiLiteLabel5.style.outline = ""
+    hiLiteImg5.style.outline = ""
+})
+
+const hiLiteLabel6 = document.querySelector('#panelLabel');
+const hiLiteImg6 = document.querySelector('#panelImg');
+hiLiteLabel6.addEventListener('mouseover', (e) => {
+    hiLiteLabel6.style.outline = "5px solid yellow"
+    hiLiteImg6.style.outline = "5px solid yellow"
+})
+hiLiteLabel6.addEventListener('mouseout', (e) => {
+    hiLiteLabel6.style.outline = ""
+    hiLiteImg6.style.outline = ""
+})
+hiLiteImg6.addEventListener('mouseover', (e) => {
+    hiLiteLabel6.style.outline = "5px solid yellow"
+    hiLiteImg6.style.outline = "5px solid yellow"
+})
+hiLiteImg6.addEventListener('mouseout', (e) => {
+    hiLiteLabel6.style.outline = ""
+    hiLiteImg6.style.outline = ""
+})
+
+const hiLiteLabel7 = document.querySelector('#iceCreamLabel');
+const hiLiteImg7 = document.querySelector('#iceCreamImg');
+hiLiteLabel7.addEventListener('mouseover', (e) => {
+    hiLiteLabel7.style.outline = "5px solid yellow"
+    hiLiteImg7.style.outline = "5px solid yellow"
+})
+hiLiteLabel7.addEventListener('mouseout', (e) => {
+    hiLiteLabel7.style.outline = ""
+    hiLiteImg7.style.outline = ""
+})
+hiLiteImg7.addEventListener('mouseover', (e) => {
+    hiLiteLabel7.style.outline = "5px solid yellow"
+    hiLiteImg7.style.outline = "5px solid yellow"
+})
+hiLiteImg7.addEventListener('mouseout', (e) => {
+    hiLiteLabel7.style.outline = ""
+    hiLiteImg7.style.outline = ""
+})
+
+const hiLiteLabel8 = document.querySelector('#catLabel');
+const hiLiteImg8 = document.querySelector('#catImg');
+hiLiteLabel8.addEventListener('mouseover', (e) => {
+    hiLiteLabel8.style.outline = "5px solid yellow"
+    hiLiteImg8.style.outline = "5px solid yellow"
+})
+hiLiteLabel8.addEventListener('mouseout', (e) => {
+    hiLiteLabel8.style.outline = ""
+    hiLiteImg8.style.outline = ""
+})
+hiLiteImg8.addEventListener('mouseover', (e) => {
+    hiLiteLabel8.style.outline = "5px solid yellow"
+    hiLiteImg8.style.outline = "5px solid yellow"
+})
+hiLiteImg8.addEventListener('mouseout', (e) => {
+    hiLiteLabel8.style.outline = ""
+    hiLiteImg8.style.outline = ""
+})
+
+const hiLiteLabel9 = document.querySelector('#glassLabel');
+const hiLiteImg9 = document.querySelector('#glassImg');
+hiLiteLabel9.addEventListener('mouseover', (e) => {
+    hiLiteLabel9.style.outline = "5px solid yellow"
+    hiLiteImg9.style.outline = "5px solid yellow"
+})
+hiLiteLabel9.addEventListener('mouseout', (e) => {
+    hiLiteLabel9.style.outline = ""
+    hiLiteImg9.style.outline = ""
+})
+hiLiteImg9.addEventListener('mouseover', (e) => {
+    hiLiteLabel9.style.outline = "5px solid yellow"
+    hiLiteImg9.style.outline = "5px solid yellow"
+})
+hiLiteImg9.addEventListener('mouseout', (e) => {
+    hiLiteLabel9.style.outline = ""
+    hiLiteImg9.style.outline = ""
+})
+
+const hiLiteLabela = document.querySelector('#oldLabel');
+const hiLiteImga = document.querySelector('#oldImg');
+hiLiteLabela.addEventListener('mouseover', (e) => {
+    hiLiteLabela.style.outline = "5px solid yellow"
+    hiLiteImga.style.outline = "5px solid yellow"
+})
+hiLiteLabela.addEventListener('mouseout', (e) => {
+    hiLiteLabela.style.outline = ""
+    hiLiteImga.style.outline = ""
+})
+hiLiteImga.addEventListener('mouseover', (e) => {
+    hiLiteLabela.style.outline = "5px solid yellow"
+    hiLiteImga.style.outline = "5px solid yellow"
+})
+hiLiteImga.addEventListener('mouseout', (e) => {
+    hiLiteLabela.style.outline = ""
+    hiLiteImga.style.outline = ""
+})
+
+const hiLiteLabelb = document.querySelector('#furnitureLabel');
+const hiLiteImgb = document.querySelector('#furnitureImg');
+hiLiteLabelb.addEventListener('mouseover', (e) => {
+    hiLiteLabelb.style.outline = "5px solid yellow"
+    hiLiteImgb.style.outline = "5px solid yellow"
+})
+hiLiteLabelb.addEventListener('mouseout', (e) => {
+    hiLiteLabelb.style.outline = ""
+    hiLiteImgb.style.outline = ""
+})
+hiLiteImgb.addEventListener('mouseover', (e) => {
+    hiLiteLabelb.style.outline = "5px solid yellow"
+    hiLiteImgb.style.outline = "5px solid yellow"
+})
+hiLiteImgb.addEventListener('mouseout', (e) => {
+    hiLiteLabelb.style.outline = ""
+    hiLiteImgb.style.outline = ""
+})
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
 }
-function mouseOut() {
-    document.getElementsByClassName("macrame").style.color = "black"
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
 }
